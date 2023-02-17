04ff0fda29a962311ca171c369cc7928df7480cd
Alejandro Ojeda Herrero <alejandro.ojeda@bosonit.com>
2023-02-17 11:01:24 +0100

    dockerv3

--- dockersubir/docker/dockerComandosNetworking.bat
+++ dockersubir/docker/dockerComandosNetworking.bat
@@ -1,14 +1,19 @@
 @echo off
 docker stop postgrescontainer
-docker stop springbootdockercontainer 
+docker stop springbootdockercontainer
+docker stop dbeivercontainer
 docker network rm postgresnetwork
 docker rm postgrescontainer
 docker rm springbootdockercontainer 
+docker rm dbeivercontainer
 docker rmi sprignbootpostgres
 
-docker network create postgresnetwork 
-docker run -d --network postgresnetwork --name postgrescontainer -e POSTGRES_PASSWORD=contrasena -e POSTGRES_USER=postgres -e POSTGRES_DB=test postgres:latest
+docker network create -d bridge --subnet 172.26.0.0/16 --gateway 172.26.0.1 postgresnetwork 
+docker run -d -p 5432:5432 --network postgresnetwork --ip 172.26.0.2 --name postgrescontainer -e POSTGRES_PASSWORD=contrasena -e POSTGRES_USER=postgres -e POSTGRES_DB=test postgres:latest
 docker build -t sprignbootpostgres:latest .
-docker run -d --network postgresnetwork  --name springbootdockercontainer sprignbootpostgres:latest
+docker run -d -p 8080:8080 --network postgresnetwork --ip 172.26.0.3 --name springbootdockercontainer sprignbootpostgres:latest
+docker run -d -p 8978:8978 --network postgresnetwork --ip 172.26.0.4 --name dbeivercontainer dbeaver/cloudbeaver:latest
+
+
 echo "Terminado"
 pause
